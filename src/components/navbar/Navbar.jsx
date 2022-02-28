bdf4cd464d741552cf7f5cd6007ed1f1860c8609
galaddirie <70884733+galaddirie@users.noreply.github.com>
2022-02-28 04:57:46 -0500

    Created NavBar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,8 +1,11 @@
-import "./navbar.css"
+import "./Navbar.css"
+
+import React, { Component } from 'react'
+import { Navbar, Nav, Container, NavItem, } from 'react-bootstrap';
+import MenuButtonToggle from "./MenuButton/MenuButton.jsx";
+
+import 'bootstrap-icons/font/bootstrap-icons.css'
 import { Link } from "react-router-dom";
-import React, { Component, useState } from 'react'
-import { Transition } from "@headlessui/react";
-import { Navbar, Nav, Container, NavItem } from 'react-bootstrap';
 class BlogNav extends Component {
     constructor(props) {
         super(props);
@@ -13,36 +16,49 @@ class BlogNav extends Component {
     render() {
         return (
 
-            <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
-                <Container className="d-flex">
-                    <Navbar.Brand href="#home">im galad</Navbar.Brand>
+            <Navbar collapseOnSelect expand="lg" variant="light">
+                <Container fluid className="d-flex mx-0" >
+                    <Navbar.Brand><Link className="nav-link " to={`/`}><img width={40} height={40} src={"img/logo.svg"} alt={"im galad logo"} />[im_galad]</Link></Navbar.Brand>
+
                     <Navbar className="order-lg-1 ms-auto me-3 d-flex">
                         <Nav className="nav-icon-container" >
-                            <Nav.Link className="nav-icon" href="#">Twitter</Nav.Link>
-                            <Nav.Link className="nav-icon" eventKey={2} href="#">
-                                Discord
+                            <Nav.Link className="nav-icon me-3" href="https://twitter.com/im_galad">
+                                <i className="bi bi-twitter"></i>
+                            </Nav.Link>
+                            <Nav.Link className="nav-icon" href="#">
+                                <i className="bi bi-discord"></i>
                             </Nav.Link>
                         </Nav>
                     </Navbar>
-                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+
+                    <MenuButtonToggle />
                     <Navbar.Collapse id="responsive-navbar-nav">
                         <Nav className="me-auto">
-                            <NavItem>
-                                <Nav.Link to="/features">Tutorials</Nav.Link>
-                                <div class="item-hover-underline"></div>
+                            <NavItem className="item-underline-container me-3 active ">
+                                <Link className="nav-link p-0" to={`/`}> Home </Link>
+                                <div className="item-hover-underline"></div>
                             </NavItem>
-                            <NavItem>
-                                <Nav.Link to="/features">Blog</Nav.Link>
-                                <div class="item-hover-underline"></div>
+                            <NavItem className="item-underline-container me-3">
+                                <Link className="nav-link p-0" to={`/tutorials`}>Tutorials</Link>
+                                <div className="item-hover-underline"></div>
                             </NavItem>
-                            <NavItem>
-                                <Nav.Link to="/features">Store</Nav.Link>
-                                <div class="item-hover-underline"></div>
+                            <NavItem className="item-underline-container me-3">
+                                <Link className="nav-link p-0" to={`/blog`}>Blog</Link>
+                                <div className="item-hover-underline"></div>
+                            </NavItem>
+                            <NavItem className="item-underline-container me-3">
+                                <Link className="nav-link p-0" to={`/portfolio`}>Portfolio</Link>
+                                <div className="item-hover-underline"></div>
+                            </NavItem>
+                            <NavItem className="item-underline-container me-3">
+                                <Link className="nav-link p-0" to={`/shop`}>shop</Link>
+                                <div className="item-hover-underline"></div>
                             </NavItem>
 
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
+
             </Navbar>
 
         )
