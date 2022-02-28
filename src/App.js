2610d67c5fc12323127eaf545c19c552b2acf7d8
galaddirie <70884733+galaddirie@users.noreply.github.com>
2022-02-28 04:59:16 -0500

    Added Page Routing

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
-import BlogNav from "./components/navbar/Navbar";
-
+import Footer from "./components/Footer/Footer";
+import Home from "./components/pages/Home/Home";
+import BlogNav from "./components/Navbar/Navbar";
+import { BrowserRouter as Router, Route, Routes, Link, } from "react-router-dom";
+import BlogList from "./components/pages/BlogList/BlogList";
+function Tuts() {
+  return (
+    <h3>tuts</h3>
+  )
+}
 
 function App() {
   return (
     <>
-      <header>
-        <BlogNav />
-      </header>
 
+      <Router>
+        <BlogNav />
+        <Routes>
+          <Route path='/' element={<Home />}></Route>
+          <Route path='/tutorials' element={<Tuts />}></Route>
+          <Route path='/blog' element={<BlogList />}></Route>
+        </Routes>
+      </Router>
       blog app
+      <Footer />
+
 
     </>
 
