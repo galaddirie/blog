7a657397aa59d4c319a53c3bbb6577fe5835d111
galaddirie <70884733+galaddirie@users.noreply.github.com>
2022-03-03 16:03:58 -0500

    Added Routes to Blog Post

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Home from "./components/pages/Home/Home";
 import BlogNav from "./components/Navbar/Navbar";
 import { BrowserRouter as Router, Route, Routes, Link, } from "react-router-dom";
 import BlogList from "./components/pages/BlogList/BlogList";
+import BlogArticle from "./components/pages/BlogList/BlogArticle";
 function Tuts() {
   return (
     <h3>tuts</h3>
@@ -19,10 +20,11 @@ function App() {
           <Route path='/' element={<Home />}></Route>
           <Route path='/tutorials' element={<Tuts />}></Route>
           <Route path='/blog' element={<BlogList />}></Route>
+          <Route path='/post' element={<BlogArticle />}></Route>
         </Routes>
+        <Footer />
       </Router>
-      blog app
-      <Footer />
+
 
 
     </>
