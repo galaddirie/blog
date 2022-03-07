b3107fccf48ef7c256926465ef7221febac0cb2d
galaddirie <70884733+galaddirie@users.noreply.github.com>
2022-03-06 22:27:26 -0500

    add page routings

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,8 @@ import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client"
 import Home from "./pages/Home/Home";
 import { BlogList, ArticlesFromTags } from "./pages/BlogList/BlogList";
 import BlogArticle from "./pages/BlogList/BlogArticle";
+import UserPage from "./pages/User/User";
+import { Container } from "react-bootstrap";
 function Tuts() {
   return (
     <h3>tuts</h3>
@@ -27,8 +29,10 @@ function App() {
             <Route path='/' element={<Home />}></Route>
             <Route path='/tutorials' element={<Tuts />}></Route>
             <Route path='/blog' element={<BlogList />}></Route>
+            <Route path='/portfolio' element={<Container className="tex-center my-auto fw-bold"><h1>Coming Soon</h1></Container>}></Route>
             <Route path='/post/:slug' element={<BlogArticle />}></Route>
             <Route path='/tag/:value' element={<ArticlesFromTags />}></Route>
+            <Route path='/user/:username' element={<UserPage />}></Route>
           </Routes>
           <Footer />
         </ApolloProvider>
