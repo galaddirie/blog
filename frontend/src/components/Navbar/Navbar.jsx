7614627d484373af89207e6abc5f7143f800ec13
galaddirie <70884733+galaddirie@users.noreply.github.com>
2022-03-06 22:27:07 -0500

    Updated NavBar

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -5,64 +5,55 @@ import { Navbar, Nav, Container, NavItem, } from 'react-bootstrap';
 import MenuButtonToggle from "./MenuButton/MenuButton.jsx";
 
 import 'bootstrap-icons/font/bootstrap-icons.css'
-import { Link } from "react-router-dom";
-class BlogNav extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            'isOpen': false
-        };
-    }
-    render() {
-        return (
+import { Link, NavLink } from "react-router-dom";
+function BlogNav() {
 
-            <Navbar collapseOnSelect expand="lg" variant="light">
-                <Container fluid className="d-flex mx-0" >
-                    <Navbar.Brand><Link className="nav-link " to={`/`}><img width={40} height={40} src={"/img/logo.svg"} alt={"im galad logo"} />[im_galad]</Link></Navbar.Brand>
+    return (
 
-                    <Navbar className="order-lg-1 ms-auto me-3 d-flex">
-                        <Nav className="nav-icon-container" >
-                            <Nav.Link className="nav-icon me-3" href="https://twitter.com/im_galad">
-                                <i className="bi bi-twitter"></i>
-                            </Nav.Link>
-                            <Nav.Link className="nav-icon" href="#">
-                                <i className="bi bi-discord"></i>
-                            </Nav.Link>
-                        </Nav>
-                    </Navbar>
+        <Navbar collapseOnSelect expand="lg" variant="light">
+            <Container fluid className="d-flex mx-0" >
+                <Navbar.Brand><Link className="nav-link " to={`/`}><img width={40} height={40} src={"/img/logo.svg"} alt={"im galad logo"} /><span>[im_galad]</span></Link></Navbar.Brand>
 
-                    <MenuButtonToggle />
-                    <Navbar.Collapse id="responsive-navbar-nav">
-                        <Nav className="me-auto">
-                            <NavItem className="item-underline-container me-3 active ">
-                                <Link className="nav-link p-0" to={`/`}> Home </Link>
-                                <div className="item-hover-underline"></div>
-                            </NavItem>
-                            {/* <NavItem className="item-underline-container me-3">
-                                <Link className="nav-link p-0" to={`/tutorials`}>Tutorials</Link>
-                                <div className="item-hover-underline"></div>
-                            </NavItem> */}
-                            <NavItem className="item-underline-container me-3">
-                                <Link className="nav-link p-0" to={`/blog`}>Blog</Link>
-                                <div className="item-hover-underline"></div>
-                            </NavItem>
-                            <NavItem className="item-underline-container me-3">
-                                <Link className="nav-link p-0" to={`/portfolio`}>Portfolio</Link>
-                                <div className="item-hover-underline"></div>
-                            </NavItem>
-                            <NavItem className="item-underline-container me-3">
-                                <a href='https://im-galad.gumroad.com/' className="nav-link p-0">shop</a>
-                                <div className="item-hover-underline"></div>
-                            </NavItem>
+                <Navbar className="order-lg-1 ms-auto me-3 d-flex">
+                    <Nav className="nav-icon-container" >
+                        <Nav.Link className="nav-icon me-3" href="https://twitter.com/im_galad">
+                            <i className="bi bi-twitter"></i>
+                        </Nav.Link>
+                        <Nav.Link className="nav-icon" href="#">
+                            <i className="bi bi-discord"></i>
+                        </Nav.Link>
+                    </Nav>
+                </Navbar>
 
-                        </Nav>
-                    </Navbar.Collapse>
-                </Container>
+                <MenuButtonToggle />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className="me-auto">
+                        <NavLink to={`/`} className={`item-underline-container nav-link me-3 ${({ isActive }) => isActive ? "active" : "inactive"} `} >
+                            <span className="nav-link p-0"> Home </span>
+                            <div className="item-hover-underline"></div>
+                        </NavLink>
+                        <NavLink to={`/blog`} className={`item-underline-container nav-link me-3 ${({ isActive }) => isActive ? "active" : "inactive"} `} >
+                            <span className="nav-link p-0"> Blog </span>
+                            <div className="item-hover-underline"></div>
+                        </NavLink>
+                        <NavLink to={`/portfolio`} className={`item-underline-container nav-link me-3 ${({ isActive }) => isActive ? "active" : "inactive"} `} >
+                            <span className="nav-link p-0"> Portfolio </span>
+                            <div className="item-hover-underline"></div>
+                        </NavLink>
+                        <a href={`https://im-galad.gumroad.com/`} target="_blank" className={`item-underline-container nav-link me-3 ${({ isActive }) => isActive ? "active" : "inactive"} `} >
+                            <span className="nav-link p-0"> shop </span>
+                            <div className="item-hover-underline"></div>
+                        </a>
 
-            </Navbar>
 
-        )
-    }
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+
+        </Navbar >
+
+    )
 }
+
 
 export default BlogNav
